c60f807921dcc55206d261bbd9c925850c29ab52
mukund <vijay.mukund7845@gmail.com>
2024-12-04 19:47:39 +0530

    Inital db schema , sql code to create it . More tables to be added later.

---
+++ createDb.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE medimap;
+
+\c medimap ;
+
+CREATE TABLE HOSPITAL(
+    id  SERIAL PRIMARY KEY,
+    hospital_name VARCHAR(32) NOT NULL,
+    addr VARCHAR(128) NOT NULL,
+    city VARCHAR(32) NOT NULL,
+    state_name VARCHAR(32) NOT NULL,
+    pincode INT NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude  DOUBLE PRECISION NOT NULL,
+    contact VARCHAR(12),
+    is_available BOOLEAN NOT NULL
+);
+
+
+
+CREATE TABLE RESOURCES(
+    id SERIAL PRIMARY KEY,
+    hospital_id INT ,
+    dept VARCHAR(32) NOT NULL,
+    is_available BOOLEAN NOT NULL,
+    resource_type VARCHAR(32),
+    quantity INT,
+    FOREIGN KEY (hospital_id) REFERENCES HOSPITAL(id)
+        ON DELETE CASCADE
+);
+
+\c postgres
